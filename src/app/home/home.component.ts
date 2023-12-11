8fd736b792eaa2eb90ce2c40630ae3cdadc5264a
juangabrieeel <juangabrielcarodiaz16@gmail.com>
2023-12-11 21:02:40 +0100

    Cambios generales y consumir la api

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { ApiService } from '../service/api.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+  data: any[] = [];
+
+  constructor(private apiService: ApiService) {
+
+  }
+
+  ngOnInit(): void {
+    this.llenarData();
+  }
+
+  llenarData() {
+    this.apiService.getData().subscribe(data => {
+      this.data = data;
+      console.log(this.data);
+    })
+  }
+}
